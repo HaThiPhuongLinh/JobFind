19cb053b33b23734047ce1505737160b4e209b3c
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2025-03-13 18:56:51 +0700

    fix something

--- src/pages/SearchResult/ListJobFiltered.jsx
+++ src/pages/SearchResult/ListJobFiltered.jsx
@@ -2,12 +2,13 @@ import { useSelector } from "react-redux";
 import JobItemv2 from "../../components/ui/JobItemv2";
 
 const ListJob = () => {
-  const jobsFiltered = useSelector((state) => state.jobs.filterJobs);
+  // const jobsFiltered = useSelector((state) => state.jobs.filterJobs);
+  const jobs = useSelector((state) => state.jobs.jobs);
   // console.log(jobsFiltered);
 
   return (
     <div>
-      {jobsFiltered.map((job) => (
+      {jobs.map((job) => (
         <JobItemv2 job={job} key={job.job_id} isApply={false} />
       ))}
     </div>
