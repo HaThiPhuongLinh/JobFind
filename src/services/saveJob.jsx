7aff35dc0e2fd772337a81ede2e6a5052aca6bc5
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2025-04-14 20:55:51 +0700

    call api get saved job

--- src/services/saveJob.jsx
+++ src/services/saveJob.jsx
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+const user = localStorage.getItem("user"); // Lấy token đã lưu sau khi login
+// chuyển token từ string sang object
+const userObject = JSON.parse(user);
+
 export const saveJob = async (jobId) => {
   try {
-    const user = localStorage.getItem("user"); // Lấy token đã lưu sau khi login
-    // chuyển token từ string sang object
-    const userObject = JSON.parse(user);
     // console.log("userObject", userObject.token);
     // console.log(
     //   `${API_URL}/savedJob/save?jobId=${jobId}&jobSeekerProfileId=${userObject?.userId}`
@@ -25,6 +26,23 @@ export const saveJob = async (jobId) => {
     return response.data;
   } catch (error) {
     console.error("Lỗi khi lưu job:", error);
+    throw error;
+  }
+};
+
+export const getListSavedJob = async () => {
+  try {
+    const result = await axios.get(
+      `${API_URL}/savedJob/listSavedJobs?jobSeekerProfileId=${userObject?.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${userObject?.token}`, // Thêm token vào header
+        },
+      }
+    );
+    return result.data;
+  } catch (error) {
+    console.error("Lỗi khi lấy danh sách job đã lưu:", error);
     throw error;
   }
 };
