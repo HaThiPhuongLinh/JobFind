5f9f922dd473ce5840a01e369a8427b1a2858755
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2025-04-21 16:39:51 +0700

    fix UI login register jsk

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
 
   // dùng useSelector để theo dõi sự thay đổi user khi logout
   let user = useSelector((state) => state.auth.user);
-  console.log(user);
+
   if (!user || user === null) {
     user = JSON.parse(localStorage.getItem("user"));
     // const token = JSON.parse(localStorage.getItem("token"));
